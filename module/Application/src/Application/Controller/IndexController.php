23203c3ce2b4b937574ba5ff6e7018f473531ed9
om choudhary <choudharyom@hotmail.com>
2013-04-26 14:41:03 +0200

    TU-Chemnitz CSS Style sheets has been implemented.
    Version is stable to use
    
    Signed-off-by: om choudhary <choudharyom@hotmail.com>

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -12,11 +12,6 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-	$writer = new Zend\Log\Writer\Stream('php://output');
-	$logger = new Zend\Log\Logger();
-	$logger->addWriter($writer);
-
-	$logger->info('Informational message');
 
     }
 }
